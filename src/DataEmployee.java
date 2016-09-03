37e54a7dc1effc99b6901127d8c79b35c94ab613
shadab0390 <ushadab.0390@gmail.com>
2016-09-03 13:56:12 +0530

    Holds Employee Data and have onlu Em[ployee Home Add

--- src/DataEmployee.java
+++ src/DataEmployee.java
@@ -3,29 +3,46 @@ import java.util.Scanner;
 public class DataEmployee {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
-		Employee[] arr = new Employee[5];
-		for(int i=0;i<5;i++)
+		Employee[] arr = new Employee[2];
+		
+		for(int i=0;i<2;i++)
 		{
 			System.out.print("Enter employee id : ");
 			int id = sc.nextInt();
 			
 			System.out.print("Enter employee name : ");
-			String name = sc.next();
+			String name = sc.next();			
 			
 			System.out.print("Enter employee salary : ");
 			int sal = sc.nextInt();
 			
-			Employee emp = new Employee(id, sal, name);
+			System.out.print("Enter house no.");
+			int house_no = sc.nextInt();
+			
+			System.out.print("Enter street name : ");
+			String street = sc.next();
+			
+			System.out.print("Enter landmark name : ");
+			String landmark = sc.next();
+			
+			System.out.print("Enter pincode ");
+			int pin = sc.nextInt();
+			
+			Address add = new Address(house_no, street,landmark, pin);
+			
+			Employee emp = new Employee(id, sal, name , add);
 			arr[i]=emp;
 		}
 		
-		System.out.println("ID  Name  Salary");
-		for(int i=0;i<5;i++)
+		System.out.println("ID     Name     Salary     House_No     Street    Landmark     Pincode");
+		for(int i=0;i<2;i++)
 		{
 			Employee emp1 = arr[i];
+			Address add = emp1.getAdd();
 			
-			System.out.println(emp1.getEmpId()+"  "+emp1.getEmpName()+"  "+emp1.getEmpSal());
+			System.out.println(emp1.getEmpId()+"     "+emp1.getEmpName()+"     "+emp1.getEmpSal()+"       "+add.getHouse_no()+"       "+add.getStreet()+"       "+add.getLandmark()+"       "+add.getPincode());
 		}
 
 	}
