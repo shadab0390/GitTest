13921a5b4d0c6c57175d9bcf7d25ea2e3c4a6ca1
shadab0390 <ushadab.0390@gmail.com>
2016-09-03 14:04:42 +0530

    Smalllest Digit in 6 Digit Number

--- src/SmallestDig.java
+++ src/SmallestDig.java
@@ -1,14 +1,14 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class SumnAvg {
+public class SmallestDig {
 
 	public static void main(String[] args) throws IOException {
 		Scanner sc =  new Scanner(System.in);
-		System.out.println("Enter a 6 Digit no. : ");
+		System.out.print("Enter a 6 Digit no. : ");
 		int num = sc.nextInt();
 		
-		SumnAvg dig =  new SumnAvg();
+		SmallestDig dig =  new SmallestDig();
 		
 		int num1 = dig.smallestDigit(num);
 		System.out.println("Smallest no.in 6 digit : "+num1);
@@ -23,7 +23,6 @@ public class SumnAvg {
 			{				
 				arr[i]=num%10;
 				num=num/10;
-				System.out.println(" Array element is "+arr[i]);
 				i++;				
 			}	
 			int temp = 0;
@@ -42,25 +41,5 @@ public class SumnAvg {
 			}
 			return arr[arr.length-1];
 	}
-	
-	
-	/*int smallestno(int[] arr1)
-	{
-		int temp = 0;
-		for(int i=0 ; i<arr1.length ; i++)
-		{
-			for(int j=i+1;j<arr1.length;j++)
-			{
-				if(arr1[i]<arr1[j])
-				{
-					temp = arr1[i];
-					arr1[i]=arr1[j];
-					arr1[j]=temp;
-				}
-			}
-		}
-		return arr1[arr1.length-1];
-	}*/
-
-}
+	}
 
